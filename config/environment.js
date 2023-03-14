0e3214da1c6f825577140ba6273e60025ec05caa
Hisham Khan <hisham.khan@trica.co>
2023-03-15 01:32:47 +0530

    set environments for production

--- config/environment.js
+++ config/environment.js
@@ -1,21 +1,23 @@
-require('dotenv').config()
+require("dotenv").config();
 const development = {
-    name:"development",
-    asset_path:"/assets",
-    db: 'test',
-    cluster_host:'hishamprofessionals',
-    key:"bir6o9z",
-    cluster_pass:process.env.CLUSTER_PASSWORD
-}
+  name: "development",
+  asset_path: "/assets",
+  db: "test",
+  cluster_host: "hishamprofessionals",
+  key: "bir6o9z",
+  cluster_pass: process.env.CLUSTER_PASSWORD,
+};
 
 const production = {
-    name:"production",
-    asset_path:"/assets",
-    db: 'test',
-    cluster_host:'hishamprofessionals',
-    key:"bir6o9z",
-    cluster_pass:process.env.CLUSTER_PASSWORD
-}
+  name: "production",
+  asset_path: "/assets",
+  db: "test",
+  cluster_host: "hishamprofessionals",
+  key: "bir6o9z",
+  cluster_pass: process.env.CLUSTER_PASSWORD,
+};
 
-
-module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
+module.exports =
+  eval(process.env.NODE_ENV) == undefined
+    ? development
+    : eval(process.env.NODE_ENV);
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
-const env = require('./environment')
+const mongoose = require("mongoose");
+const env = require("./environment");
 
-let uri = `mongodb+srv://${env.cluster_host}:${env.cluster_pass}@cluster0.${env.key}.mongodb.net/?retryWrites=true&w=majority`
+let uri = `mongodb+srv://${env.cluster_host}:${env.cluster_pass}@cluster0.${env.key}.mongodb.net/?retryWrites=true&w=majority`;
 
-main().catch((err) => console.log('Error in connecting Database:\n', err))
+main().catch((err) => console.log("Error in connecting Database:\n", err));
 
 async function main() {
-  await mongoose.connect(uri)
+  await mongoose.connect(uri);
 }
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.once('open', () => {
-  console.log('connected to MongoDb ::', env.db)
-})
+db.once("open", () => {
+  console.log("connected to MongoDb ::", env.db);
+});
 
-module.exports = db
+module.exports = db;
