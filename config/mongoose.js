fdf2ff045038953495b779c5ba609d721b0da3ee
Hisham Khan <hisham.khan@trica.co>
2023-03-15 01:35:51 +0530

    set environments for production

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,8 @@ const env = require("./environment");
 
 let uri = `mongodb+srv://${env.cluster_host}:${env.cluster_pass}@cluster0.${env.key}.mongodb.net/?retryWrites=true&w=majority`;
 
+console.log("uri", uri);
+
 main().catch((err) => console.log("Error in connecting Database:\n", err));
 
 async function main() {
